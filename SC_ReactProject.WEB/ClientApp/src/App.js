57dbcf6e3b1a44bc51733c17f85a1a463a686874
Aleksandra Umnykh <alexandraumnykh@gmail.com>
2022-01-20 10:51:45 +0300

    - added sessionStorage;

--- SC_ReactProject.WEB/ClientApp/src/App.js
+++ SC_ReactProject.WEB/ClientApp/src/App.js
@@ -1,4 +1,4 @@
-﻿import React, { Component, useState } from 'react';
+﻿import React, { Component, useState, useContext } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 
@@ -6,6 +6,7 @@ import { Layout } from './components/Layout';
 import './styles.css'
 import { EmployeesList } from './components/EmployeesList';
 import LogInForm from './components/LogInForm';
+import { LogInContext } from './helper/Context';
 
 export default function App() {
 
@@ -16,24 +17,29 @@ export default function App() {
 
   const [currentUser, setCurrentUser] = useState({email: "", password: ""});
 
+  const [loggedIn, setLoggedIn] = useState(false);
+
   const login = details => {
       if (details.email == adminUser.email && details.password == adminUser.password){
-      setCurrentUser({
-        email: details.email,
-        password: details.password
-      });
+        setCurrentUser({
+            email: details.email,
+            password: details.password
+        });
+        sessionStorage.setItem('currentUserIsLoggedIn', true);
     }
   }
 
+  const isLoggedIn = sessionStorage.getItem('currentUserIsLoggedIn');
+
   return (
-    <div className='App'>
+    <LogInContext.Provider value={{ loggedIn: false, setLoggedIn }} className='App'>
       {
-        (currentUser.email != "") ? (
+        isLoggedIn ? (
           <div>
             <EmployeesList />
           </div>
         ) : <LogInForm login={login} />
       }
-    </div>
+    </LogInContext.Provider>
   );
 }
